09295b7c934f05624a3b33154fbfea921c0a8c63
cyanray <a484599279@hotmail.com>
2021-03-17 09:54:34 +0800

    新增: 查空教室支持多个时间条件、多个楼层条件;

--- src/CmdFreeClassroom.cpp
+++ src/CmdFreeClassroom.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <string_view>
 #include <sstream>
+#include <vector>
+#include <unordered_set>
 #include <glog/logging.h>
 #include <algorithm>
 #include "main.h"
@@ -26,8 +28,8 @@ void CmdFreeClassroom(Message m)
 		string date = GetCurrentDate();
 		string campus_id = "02";
 		string building_id = "0201";
-		int floor = 0;  //0为全楼
-		Jw::FreeTime free_time = Jw::FreeTime::AllDay;
+		vector<Jw::FreeTime> free_times;
+		unordered_set<int> floors;
 
 		if (msg_view.find("明天") != msg_view.npos)
 		{
@@ -52,70 +54,98 @@ void CmdFreeClassroom(Message m)
 			building_id = "0102";
 		}
 
-		if (msg_view.find("全天") != msg_view.npos)
-		{
-			free_time = Jw::FreeTime::AllDay;
-		}
+		//if (msg_view.find("全天") != msg_view.npos)
+		//{
+		//	free_time = Jw::FreeTime::AllDay;
+		//}
 
 		if (msg_view.find("上午") != msg_view.npos)
 		{
-			free_time = Jw::FreeTime::AM;
+			free_times.push_back(Jw::FreeTime::AM);
 		}
 
 		if (msg_view.find("下午") != msg_view.npos)
 		{
-			free_time = Jw::FreeTime::PM;
+			free_times.push_back(Jw::FreeTime::PM);
 		}
 
 		if (msg_view.find("晚上") != msg_view.npos)
 		{
-			free_time = Jw::FreeTime::Night;
+			free_times.push_back(Jw::FreeTime::Night);
 		}
 
 		if (msg_view.find("1楼") != msg_view.npos || msg_view.find("一楼") != msg_view.npos)
 		{
-			floor = 1;
+			floors.insert(1);
 		}
 		if (msg_view.find("2楼") != msg_view.npos || msg_view.find("二楼") != msg_view.npos)
 		{
-			floor = 2;
+			floors.insert(2);
 		}
 		if (msg_view.find("3楼") != msg_view.npos || msg_view.find("三楼") != msg_view.npos)
 		{
-			floor = 3;
+			floors.insert(3);
 		}
 		if (msg_view.find("4楼") != msg_view.npos || msg_view.find("四楼") != msg_view.npos)
 		{
-			floor = 4;
+			floors.insert(4);
 		}
 		if (msg_view.find("5楼") != msg_view.npos || msg_view.find("五楼") != msg_view.npos)
 		{
-			floor = 5;
+			floors.insert(5);
 		}
 		if (msg_view.find("6楼") != msg_view.npos || msg_view.find("六楼") != msg_view.npos)
 		{
-			floor = 6;
+			floors.insert(6);
 		}
 		if (msg_view.find("7楼") != msg_view.npos || msg_view.find("七楼") != msg_view.npos)
 		{
-			floor = 7;
+			floors.insert(7);
 		}
 		if (msg_view.find("8楼") != msg_view.npos || msg_view.find("八楼") != msg_view.npos)
 		{
-			floor = 8;
+			floors.insert(8);
 		}
 		if (msg_view.find("9楼") != msg_view.npos || msg_view.find("九楼") != msg_view.npos)
 		{
-			floor = 9;
+			floors.insert(9);
 		}
 
-
-		auto result = JwApi.GetFreeClassroom(date, free_time, campus_id, building_id);
+		// 全天时间段的教室是特殊的，所有时间条件它都满足
+		free_times.push_back(Jw::FreeTime::AllDay);
+		// 下面两个循环用于筛选同时满足其他时间段条件的教室 (顺便将楼层按照字典序排序)
+		vector<Jw::FreeClassroom> result;
+		map<string, pair<int, Jw::FreeClassroom>> container;
+		for (auto&& free_time : free_times)
+		{
+			auto tmp = JwApi.GetFreeClassroom(date, free_time, campus_id, building_id);
+			for (const auto& classroom : tmp)
+			{
+				if (container.contains(classroom.ClassroomName))
+				{
+					container[classroom.ClassroomName].first += 1;
+				}
+				else
+				{
+					container.insert(make_pair(classroom.ClassroomName, make_pair(1, classroom)));
+				}
+			}
+		}
+		for (auto&& t : container)
+		{
+			if (t.second.first >= free_times.size() - 1)
+			{
+				result.push_back(t.second.second);
+			}
+		}
 
 		//选择了楼层选项，则进行楼层筛选
-		if (floor != 0)
+		if (floors.size() != 0)
 		{
-			std::erase_if(result, [&](const Jw::FreeClassroom& c) { return c.Floor == -1 || c.Floor != floor;  });
+			std::erase_if(result, [&](const Jw::FreeClassroom& c) 
+				{ 
+					return c.Floor == -1 || !floors.contains(c.Floor);  
+				});
 		}
 
 		// 最多显示 100 个教室(20条消息)
@@ -123,12 +153,12 @@ void CmdFreeClassroom(Message m)
 
 		if (result.empty())
 		{
-			m.Reply(MessageChain().Plain("没有找到 " + date + " " + FreeTimeText[(int)(free_time)] + "空闲的教室"));
+			m.Reply(MessageChain().Plain("没有找到 " + date /*+ " " + FreeTimeText[(int)(free_times)] + "空闲的教室"*/));
 		}
 		else
 		{
 			m.Reply(MessageChain()
-				.Plain(date + " " + FreeTimeText[(int)(free_time)] + "空闲的教室有" + to_string(len) + "个："));
+				.Plain(date + " " + /*FreeTimeText[(int)(free_times)] + */"空闲的教室有" + to_string(len) + "个："));
 		}
 
 		MessageChain mc;
