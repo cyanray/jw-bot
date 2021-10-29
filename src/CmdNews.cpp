a0ec17419c05062e4d1a907e0e0da5f71a02b702
cyanray <a484599279@hotmail.com>
2021-10-29 17:43:04 +0800

    重构教务新闻;

--- src/CmdNews.cpp
+++ src/CmdNews.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <glog/logging.h>
+#include <fmt/core.h>
 #include "main.h"
 using namespace std;
 using namespace Cyan;
@@ -9,23 +10,21 @@ void CmdNews(Message m)
 {
 	if (m.MessageChain.GetPlainTextFirst() != "教务新闻") return;
 
-	LOG(INFO) << "[" << m.Sender.ToInt64() << "] 使用 [教务新闻] 指令";
+	LOG(INFO) << "[" << m.Sender << "] 使用 [教务新闻] 指令";
 
 	try
 	{
 		for (const auto& n : NewsDb.GetLatestNews())
 		{
 			MessageChain news;
-			news.Plain("标题: " + n.title + "\n");
-			news.Plain("网址: " + n.url + "\n");
-			news.Plain("时间: " + n.time);
+			news.Plain(fmt::format("标题: {}\n网址: {}\n时间: {}", n.title, n.url, n.time));
 			m.Reply(news);
 			MiraiBot::SleepMilliseconds(200);
 		}
 	}
 	catch (const std::exception& ex)
 	{
-		LOG(ERROR) << "[" << m.Sender.ToInt64() << "] 使用 [教务新闻] 指令时出现异常: " << ex.what();
+		LOG(ERROR) << "[" << m.Sender << "] 使用 [教务新闻] 指令时出现异常: " << ex.what();
 		try
 		{
 			m.Reply(MessageChain().Plain("出现错误："s + ex.what()));
