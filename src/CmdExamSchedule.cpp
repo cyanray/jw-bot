61fa226069b052efe756a2f218fbc4278699b33a
cyanray <a484599279@hotmail.com>
2021-10-29 17:26:04 +0800

    重构考试安排;

--- src/CmdExamSchedule.cpp
+++ src/CmdExamSchedule.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <glog/logging.h>
+#include <fmt/core.h>
 #include "main.h"
 using namespace std;
 using namespace Cyan;
@@ -9,25 +10,25 @@ using namespace cyanray;
 void CmdExamSchedule(Message m)
 {
 	if (m.MessageChain.GetPlainTextFirst() != "考试安排") return;
-	LOG(INFO) << "[" << m.Sender.ToInt64() << "] 使用 [考试安排] 指令";
+	LOG(INFO) << "[" << m.Sender << "] 使用 [考试安排] 指令";
 
 	string schoolId = UserDb.GetSid(m.Sender);
 	if (schoolId.empty())
 	{
-		LOG(INFO) << "[" << m.Sender.ToInt64() << "] 使用 [考试安排] 指令时出现错误: 没有找到学号!";
+		LOG(INFO) << "[" << m.Sender << "] 使用 [考试安排] 指令时出现错误: 没有找到学号!";
 		m.Reply(MessageChain().Plain(UNKNOWN_SCHOOL_ID_MSG));
 		return;
 	}
 
 	try
 	{
-		LOG(INFO) << "[" << m.Sender.ToInt64() << "] 获取 [" << schoolId << "] 的考试安排...";
+		LOG(INFO) << "[" << m.Sender << "] 获取 [" << schoolId << "] 的考试安排...";
 		vector<Jw::ExamSchedule> es = JwApi.GetExamSchedule(schoolId);
-		LOG(INFO) << "[" << m.Sender.ToInt64() << "] 获取到 " << es.size() << " 个考试安排";
+		LOG(INFO) << "[" << m.Sender << "] 获取到 " << es.size() << " 个考试安排";
 		MessageChain mc;
 		if (!es.empty())
 		{
-			mc.Plain("查询到 ").Plain(es.size()).Plain(" 个考试安排\n");
+			mc.Plain(fmt::format("查询到 {} 个考试安排", es.size()));
 		}
 		else
 		{
@@ -37,27 +38,23 @@ void CmdExamSchedule(Message m)
 		int i = 1;
 		for (const auto& item : es)
 		{
-			LOG(INFO) << "[" << m.Sender.ToInt64() << "] 考试安排: " << item.CourseName << ", " << item.ExamRoom << ", " << item.ChineseTime;
+			LOG(INFO) << "[" << m.Sender << "] 考试安排: " << item.CourseName << ", " << item.ExamRoom << ", " << item.ChineseTime;
 			MessageChain mc;
-			mc
-				.Plain(i++).Plain(". ").Plain(item.CourseName).Plain("\n")
-				.Plain("教室：").Plain(item.ExamRoom).Plain("\n")
-				.Plain("时间: ").Plain(item.ChineseTime).Plain("\n")
-				.Plain(item.Time);
+			mc.Plain(fmt::format("{}. {}\n教室: {}\n时间: {}\n{}", i++, item.CourseName, item.ExamRoom, item.ChineseTime, item.Time));
 			m.Reply(mc);
 		}
-
+		m.Reply(MessageChain().Plain("机器人无法查询座位号! 记得到教务网查询座位号哦！"));
 	}
 	catch (const std::exception& ex)
 	{
-		LOG(ERROR) << "[" << m.Sender.ToInt64() << "] 使用 [考试安排] 指令时出现异常: " << ex.what();
+		LOG(ERROR) << "[" << m.Sender << "] 使用 [考试安排] 指令时出现异常: " << ex.what();
 		try
 		{
 			m.Reply(MessageChain().Plain("出现错误："s + ex.what()));
 		}
 		catch (const exception& ex)
 		{
-			LOG(ERROR) << "[" << m.Sender.ToInt64() << "] 使用 [考试安排] 指令时出现异常: " << ex.what();
+			LOG(ERROR) << "[" << m.Sender << "] 使用 [考试安排] 指令时出现异常: " << ex.what();
 		}
 	}
 
