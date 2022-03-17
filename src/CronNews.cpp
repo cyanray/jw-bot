8d5290732f1dc2a86873856027a7220b8d7bfc8c
cyanray <a484599279@hotmail.com>
2022-03-17 11:39:05 +0800

    更新教务新闻推送逻辑;

--- src/CronNews.cpp
+++ src/CronNews.cpp
@@ -16,61 +16,68 @@ void CronJobNews(Cyan::MiraiBot& bot)
 		try
 		{
 			LOG(INFO) << "发起查询教务新闻的网络请求...";
+			vector<News> newsList;
 			HTTP http;
 			auto resp = http.Get("http://jw.cqjtu.edu.cn/jxxx/tzgg1.htm");
-			if (resp.Ready && resp.StatusCode == 200)
+			if (!resp.Ready || resp.StatusCode != 200) continue;
+
+			HTMLDoc html;
+			html.Parse(resp.Content);
+			// /html/body/div[3]/div/div/div[2]/div/div[2]/div/div/div/div[1]/ul
+			auto list =
+				html["html"]["body"]
+				["div"][2]
+				["div"]["div"]["div"][1]
+				["div"]["div"][1]
+				["div"]["div"]["div"]["div"][0]["ul"];
+			auto tables = list.SearchByTagName("dt");
+			regex pattern(R"((?:\.\.)?(.+))", regex::ECMAScript);
+			for (auto& t : tables)
 			{
-				HTMLDoc html;
-				html.Parse(resp.Content);
-				// /html/body/div[3]/div/div/div[2]/div/div[2]/div/div/div/div[1]/ul
-				auto list =
-					html["html"]["body"]
-					["div"][2]
-					["div"]["div"]["div"][1]
-					["div"]["div"][1]
-					["div"]["div"]["div"]["div"][0]["ul"];
-				auto tables = list.SearchByTagName("dt");
-				regex pattern(R"((?:\.\.)?(.+))", regex::ECMAScript);
-				for (auto& t : tables)
+				HTMLDoc a = t["a"];
+				string tmp = a.GetAttribute("href");
+				std::smatch matches;
+				if (regex_search(tmp, matches, pattern))
 				{
-					HTMLDoc a = t["a"];
-					string tmp = a.GetAttribute("href");
-					std::smatch matches;
-					if (regex_search(tmp, matches, pattern))
-					{
-						tmp = matches.str(1);	
-					}
-					string url = "http://jw.cqjtu.edu.cn" + tmp;
-					string title = a["span"][0].GetInner();
-					string date = a["span"][1].GetInner();
-					LOG(INFO) << "查询到新闻：" << date << " 《" << title << "》";
-					if (!NewsDb.Exist(title))
-					{
-						LOG(INFO) << "新闻 《" << title << "》不在数据库，广播给订阅用户...";
-						NewsDb.Add(title, date, url);
-						vector<QQ_t> users = UserDb.GetNewsSubscribers();
-						for (auto user : users)
-						{
-							try
-							{
-								bot.SendMessage(user, MessageChain().Plain("有新的教务网新闻").Face(rand() % 30));
-								MessageChain news;
-								news.Plain("标题: " + title + "\n");
-								news.Plain("网址: " + url + "\n");
-								news.Plain("时间: " + date);
-								bot.SendMessage(user, news);
-								LOG(INFO) << "发送教务新闻给 [" << user << "] 成功!";
-							}
-							catch (const exception& ex)
-							{
-								LOG(ERROR) << "发送教务新闻给 [" << user << "] 失败: " << ex.what();
-							}
-							MiraiBot::SleepSeconds(10);
-						}
-
-					}
-
+					tmp = matches.str(1);
 				}
+				News news;
+				news.url = "http://jw.cqjtu.edu.cn" + tmp;
+				news.title = a["span"][0].GetInner();
+				news.time = a["span"][1].GetInner();
+				LOG(INFO) << "查询到新闻：" << news.time << " 《" << news.title << "》";
+				if (!NewsDb.Exist(news.title))
+				{
+					newsList.emplace_back(std::move(news));
+				}
+			}
+			vector<QQ_t> users = UserDb.GetNewsSubscribers();
+			for (auto user : users)
+			{
+				bot.SendMessage(user, MessageChain().Plain("有新的教务网新闻").Face(rand() % 30));
+				for (auto&& news : newsList)
+				{
+					try
+					{
+						MessageChain newsTemplate;
+						newsTemplate.Plain("标题: " + news.title + "\n");
+						newsTemplate.Plain("网址: " + news.url + "\n");
+						newsTemplate.Plain("时间: " + news.time);
+						bot.SendMessage(user, newsTemplate);
+						LOG(INFO) << "发送教务新闻给 [" << user << "] 成功!";
+					}
+					catch (const exception& ex)
+					{
+						LOG(ERROR) << "发送教务新闻给 [" << user << "] 失败: " << ex.what();
+					}
+					MiraiBot::SleepSeconds(2);
+				}
+				MiraiBot::SleepSeconds(10);
+			}
+			// 全部发送完成再加入数据库
+			for (auto&& news : newsList)
+			{
+				NewsDb.Add(news.title, news.time, news.url);
 			}
 
 		}
